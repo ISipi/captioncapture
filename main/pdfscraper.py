68c617702c1b6c6582bbb2ac5409b2faa2580129
ISipi <ilkkasipila@yahoo.com>
2021-06-26 15:58:45 +0100

    Added multithreading

--- main/pdfscraper.py
+++ main/pdfscraper.py
@@ -5,24 +5,50 @@ import argparse
 import requests
 import os
 import time
-import random
+import concurrent.futures
+
+MAX_THREADS = 10
+
+
+def run_img_extraction(local_folder):
+    """
+    Go to the local_folder and open PDFs
+    :param local_folder:
+    :return:
+    """
+
+    for i in os.listdir(local_folder):
+        if '.pdf' == i[-4:]:
+            pdf_file = os.path.join(local_folder, i)
+
+            """
+            
+            Do something to the pdf file:
+            
+            """
+
+    pass
+
+
+def downloaded(pdf_link):
+    r = requests.get(pdf_link, allow_redirects=True)
+    num_items_in_folder = len([i for i in os.listdir(output_folder)])
+
+    with open(f'{output_folder}/downloaded_pdf_{num_items_in_folder}.pdf', 'wb') as pdf:
+        pdf.write(r.content)
+    time.sleep(0.25)
 
 
 def scrape_a_scholar(query, lan, output_folder, num_pages):
-
     # sanity check - make sure that output folder exists, and if not, create it:
     if os.path.exists(output_folder) is False:
         os.makedirs(output_folder)
-
-    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) '
-                            'Version/9.0.2 Safari/601.3.9', 'CloudFront-Is-Desktop-Viewer': 'true'}
 
     failed_downloads = 0
     success = 0
 
     # base url:
-    url = f'https://scholar.google.com/scholar?'  # hl={lan}&as_sdt=0%2C5&q={query}
-
+    url = 'https://scholar.google.com/scholar?'
     # loop through pages:
     for i in range(num_pages):
         # change payload based on page - starting page doesn't have 'start' payload:
@@ -30,38 +56,31 @@ def scrape_a_scholar(query, lan, output_folder, num_pages):
             payload = {'q': str(query), 'hl': str(lan)}
         else:
             payload = {'q': str(query), 'hl': str(lan), 'start': str(i*10)}
-        response = requests.get(url, headers=headers, params=payload)
+        response = requests.get(url, params=payload)
         soup = BeautifulSoup(response.content,'lxml')
-
+        downloadable_pdfs = []
         for span in soup.find_all('span', {'class': 'gs_ctg2'}):
-            nap = random.randint(0, 5)
-            time.sleep(nap)
+
             # if [PDF] text is given for a link, check the link's headers
             if span.text == "[PDF]":
                 pdf_link = span.parent.get('href')
-                #if "academia.edu" in span.parent.text:
 
                 r = requests.get(pdf_link, stream=True)
                 # if the header Content-Type is 'application/pdf, we know it can be downloaded.
                 downloadable = 'application/pdf' in r.headers.get('Content-Type', '')
-                print(downloadable)
-                if downloadable:
-
+                attachment = '.pdf' in r.headers.get('Content-Disposition', '')
+                print(r.headers)
+                if downloadable or attachment:
+                    downloadable_pdfs.append(pdf_link)
                     # use requests to download a file
-                    r = requests.get(pdf_link, allow_redirects=True)
                     success += 1
-                    num_items_in_folder = len([i for i in os.listdir(output_folder)])
-
-                    with open(f'{output_folder}/downloaded_pdf_{num_items_in_folder}.pdf', 'wb') as pdf:
-                        pdf.write(r.content)
-
                 else:
-                    """
-                    r = requests.get(pdf_link, allow_redirects=True)
-                    new_soup = BeautifulSoup(r.content, 'lxml')
-
-                    print(new_soup.prettify())"""
                     failed_downloads += 1
+
+        # use threading to download files simultaneously
+        threads = min(MAX_THREADS, len(downloadable_pdfs))
+        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+            executor.map(downloaded, downloadable_pdfs)
         time.sleep(5)
 
     print(f"Number of failed downloads: {failed_downloads}. Successes: {success}")
@@ -74,14 +93,21 @@ if __name__ == "__main__":
     parser.add_argument("--lan", type=str, default='en', help="Choose a language, default 'en'")
     parser.add_argument("--output_folder", type=str, default="../downloaded_pdfs/",
                         help="Full path to the output folder. Default is current directory.")
-    parser.add_argument("--num_pages", type=int, default=1, help="How many pages to search through?")
+    parser.add_argument("--num_pages", type=int, default=5, help="How many pages to search through?")
+    parser.add_argument("--local_folder", type=str, default='',
+                        help="Only provide an argument if you want to perform image and caption extraction on PDFs "
+                             "locally. Full path required.")
 
     """ TODO: 
         let user to decide whether to save the PDFs
     """
 
     args = parser.parse_args()
-    query, lan, output_folder, num_pages = args.query, args.lan, args.output_folder, args.num_pages
+    query, lan, output_folder, num_pages, local_folder = args.query, args.lan, args.output_folder, args.num_pages, \
+                                                         args.local_folder
 
-    # scrape PDFs:
-    scrape_a_scholar(query, lan, output_folder, num_pages)
+    if local_folder:
+        run_img_extraction(local_folder)
+    else:
+        # scrape PDFs:
+        scrape_a_scholar(query, lan, output_folder, num_pages)
